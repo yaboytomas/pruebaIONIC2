2af982889593e1ac8a98648df956029f8d36431f
Tomas <tomasszabo94@gmail.com>
2025-07-19 15:34:00 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EVU1_SZABO_TOMAS',
+  webDir: 'dist'
+};
+
+export default config;
